91dc9c3449f45d603dfa56415471ccdbdb649edb
Tero Saarikivi <saarikivit@gmail.com>
2025-01-13 11:09:48 +0200

    chore: add support and feedback page

---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,20 @@
+<article class="prose prose-zinc lg:prose-lg">
+	<h1>Support & Feedback</h1>
+
+	<p>Need help or have a suggestion? Contact us at support@sonofield.com. We are here to assist!</p>
+</article>
+
+<style>
+	article {
+		margin: 0 auto;
+		padding: 64px 16px;
+	}
+
+	h1 {
+		color: #eaeaea;
+	}
+
+	article {
+		color: #bebebe;
+	}
+</style>
